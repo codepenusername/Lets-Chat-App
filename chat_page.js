58f47fd8ca77d97ba9abc3ed0a19a6d59c17c8ee
Ayush <100988794+codepenusername@users.noreply.github.com>
2023-03-19 17:32:39 +0530

    Add files via upload

---
+++ chat_page.js
@@ -0,0 +1,68 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBiTTLuM101hoK3lr5-AI7_uIrfq2QbWpM",
+  authDomain: "my-projects-19216.firebaseapp.com",
+  databaseURL: "https://my-projects-19216-default-rtdb.firebaseio.com",
+  projectId: "my-projects-19216",
+  storageBucket: "my-projects-19216.appspot.com",
+  messagingSenderId: "336940229858",
+  appId: "1:336940229858:web:b27fcc2bcc372084a3eabb",
+  measurementId: "G-86FF1M2L29"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+	user_name = localStorage.getItem("user_name");
+	room_name = localStorage.getItem("room_name");
+
+function send()
+{
+  msg = document.getElementById("msg").value;
+  firebase.database().ref(room_name).push({
+    name:user_name,
+    message:msg,
+    like:0
+   });
+
+  document.getElementById("msg").value = "";
+}
+
+function getData()
+ { firebase.database().ref("/"+room_name).on('value', function(snapshot) { document.getElementById("output").innerHTML = ""; snapshot.forEach(function(childSnapshot) { childKey  = childSnapshot.key; childData = childSnapshot.val(); if(childKey != "purpose") {
+         firebase_message_id = childKey;
+         message_data = childData;
+//Start code
+         console.log(firebase_message_id);
+	       console.log(message_data);
+	       name = message_data['name'];
+	       message = message_data['message'];
+         like = message_data['like'];
+         name_with_tag = "<h4> "+ name +"<img class='user_tick' src='tick.png'></h4>";
+         message_with_tag = "<h4 class='message_h4'>" + message + "</h4>";
+         like_button ="<button class='btn btn-warning' id="+firebase_message_id+" value="+like+" onclick='updateLike(this.id)'>";
+         span_with_tag = "<span class='glyphicon glyphicon-thumbs-up'> Like: "+ like +"</span></button><hr>";
+
+        row = name_with_tag + message_with_tag +like_button + span_with_tag;       
+        document.getElementById("output").innerHTML += row;
+//End code
+      } });  }); }
+getData();
+
+function updateLike(message_id)
+{
+  console.log("clicked on like button - " + message_id);
+	button_id = message_id;
+	likes = document.getElementById(button_id).value;
+	updated_likes = Number(likes) + 1;
+	console.log(updated_likes);
+
+	firebase.database().ref(room_name).child(message_id).update({
+		like : updated_likes  
+	 });
+
+}
+
+function logout() {
+localStorage.removeItem("user_name");
+localStorage.removeItem("room_name");
+window.location.replace("index.html");
+}
---
+++ chat_room.js
@@ -0,0 +1,53 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBiTTLuM101hoK3lr5-AI7_uIrfq2QbWpM",
+  authDomain: "my-projects-19216.firebaseapp.com",
+  databaseURL: "https://my-projects-19216-default-rtdb.firebaseio.com",
+  projectId: "my-projects-19216",
+  storageBucket: "my-projects-19216.appspot.com",
+  messagingSenderId: "336940229858",
+  appId: "1:336940229858:web:b27fcc2bcc372084a3eabb",
+  measurementId: "G-86FF1M2L29"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+user_name=localStorage.getItem("user_name");
+document.getElementById("user_name").innerHTML = "Welcome " + user_name + "!";
+
+function addRoom()
+{
+  room_name = document.getElementById("room_name").value;
+
+  firebase.database().ref("/").child(room_name).update({
+    purpose : "adding room name"
+  });
+
+    localStorage.setItem("room_name", room_name);
+    
+    window.location = "chat_page.html";
+}
+
+function getData() {  firebase.database().ref("/").on('value', function(snapshot) { document.getElementById("output").innerHTML = ""; snapshot.forEach(function(childSnapshot) { childKey  = childSnapshot.key;
+       Room_names = childKey;
+       console.log("Room Name - " + Room_names);
+      row = "<div class='room_name' id="+Room_names+" onclick='redirectToRoomName(this.id)' >#"+ Room_names +"</div><hr>";
+      document.getElementById("output").innerHTML += row;
+    });
+  });
+
+}
+
+getData();
+
+function redirectToRoomName(name)
+{
+  console.log(name);
+  localStorage.setItem("room_name", name);
+    window.location = "chat_page.html";
+}
+
+function logout() {
+localStorage.removeItem("user_name");
+localStorage.removeItem("room_name");
+    window.location = "index.html";
+}
